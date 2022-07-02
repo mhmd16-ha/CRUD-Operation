6c6a174a54738639a4e19d9bd59c095d344d5375
MHMD HAMOUDA <MHMD HAMOUDA@DESKTOP-DLSBE4V>
2022-07-02 16:25:53 +0200

    Add project files.

---
+++ Test/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test.Models
+{
+    [Table("Department",Schema ="HR")]
+    public class Department
+    {
+        [Key]
+        [Display(Name ="ID")]
+        public int DepartmentId { get; set; }
+        [Required]
+        [Display(Name = "Name")]
+        [Column (TypeName ="varchar(200)")]
+        public string? DepartmentName { get; set; }
+    }
+}
---
+++ Test/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Test.Models
+{
+    [Table("Employee",Schema ="HR")]
+    public class Employee
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }
+    }
+}
